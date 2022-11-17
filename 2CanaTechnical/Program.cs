e50067ce49768cce0064687f21fb1353c7b2c0e8
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-11-17 03:31:47 +0200

    Add project files.

---
+++ 2CanaTechnical/Program.cs
@@ -0,0 +1,13 @@
+﻿using _2CanaTechnical.Questions;
+
+// -- instantiating class object
+QuestionA questionA = new QuestionA();
+QuestionB questionB = new QuestionB();
+
+// -- question a
+questionA.ReadSalesAmt();
+questionA.Display();
+
+// -- quesion b
+
+
---
+++ 2CanaTechnical/Questions/QuestionA.cs
@@ -0,0 +1,57 @@
+﻿namespace _2CanaTechnical.Questions
+{
+    internal class QuestionA
+    {
+        List<double> transactions = new List<double>();
+
+        public void ReadSalesAmt()
+        {
+            double amt = 0;
+
+            Console.Write("Enter in amount R: ");
+            amt = Convert.ToDouble(Console.ReadLine());
+            
+            while (amt != 0)
+            {
+                transactions.Add(amt);
+                
+                Console.Write("Enter in amount R: ");
+                amt = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+
+        public double CalculateTotalSales()
+        {
+            double total = 0;
+
+            foreach (double i in transactions)
+            {
+                total += i;
+            }
+
+            return total;
+        }
+
+
+        public double CalculateDiscount(double total)
+        {
+             double DISCOUNT_RATE = 0.20;
+
+            return (total * DISCOUNT_RATE) ;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\n-----------------------Transactions-------------------------");
+            foreach (double i in transactions)
+            {
+                Console.WriteLine("Amount: R{0:0.00}", i);
+            }
+            Console.WriteLine("\n------------------------Totals------------------------------");    
+            Console.WriteLine("Total no. of items: " + transactions.Count);
+            Console.WriteLine("Total value of sales: R {0:0.00}" , CalculateTotalSales());
+            Console.WriteLine("Total discount: R {0:0.00}", CalculateDiscount(CalculateTotalSales()));
+        }
+    }
+}
